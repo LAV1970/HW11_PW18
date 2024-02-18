7a9e5900bc725195b74c689e15dd4e1533b7cb04
lomakina0612 <lomakina0612@gmail.com>
2024-02-18 12:28:08 +0100

    18_02_13

--- alembic/env.py
+++ alembic/env.py
@@ -7,46 +7,38 @@ from logging.config import fileConfig
 from multiprocessing import pool
 from sqlalchemy import engine_from_config
 from alembic import context
-from models import Base
+from models import Base  # Исправляем инструкцию импорта
+from sqlalchemy.pool import NullPool
 
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# Это объект Alembic Config, который предоставляет
+# доступ к значениям в используемом файле .ini.
 config = context.config
 fileConfig(config.config_file_name)
-target_metadata = None
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+# Интерпретация файла конфигурации для логирования Python.
+# Эта строка, в основном, настраивает логгеры.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-from models import YourBase
-
+# Используйте Base.metadata для target_metadata
 target_metadata = Base.metadata
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
+# Другие значения из конфигурации, определенные потребностями env.py,
+# могут быть получены:
 # my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+# ... и так далее.
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    """Выполняет миграции в режиме 'offline'.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+    Это настраивает контекст только с URL
+    и без Engine, хотя Engine также допустим
+    здесь. Пропуская создание Engine
+    нам даже не нужен DBAPI.
 
-    Calls to context.execute() here emit the given string to the
-    script output.
+    Вызовы context.execute() здесь отправляют данный текст в
+    вывод сценария.
 
     """
     url = config.get_main_option("sqlalchemy.url")
@@ -62,16 +54,16 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    """Выполняет миграции в режиме 'online'.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
+    В этом сценарии нам нужно создать Engine
+    и связать соединение с контекстом.
 
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+        poolclass=NullPool,
     )
 
     with connectable.connect() as connection:
