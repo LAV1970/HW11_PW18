7ace8c13d3d065f64c0830a459f44dfbc9317d63
lomakina0612 <lomakina0612@gmail.com>
2024-02-18 07:07:34 +0100

    18_02_07

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, Session, sessionmaker
@@ -37,6 +37,61 @@ class ContactValidator(BaseModel):
     name: str
     phone: str
     email: str
+
+
+# Ручка для создания нового контакта
+@app.post("/contacts/", response_model=ContactValidator)
+def create_contact(contact: ContactValidator, db: Session = Depends(get_db)):
+    new_contact = Contact(**contact.dict())
+    db.add(new_contact)
+    db.commit()
+    db.refresh(new_contact)
+    return new_contact
+
+
+# Ручка для получения списка всех контактов
+@app.get("/contacts/", response_model=List[ContactValidator])
+def get_all_contacts(db: Session = Depends(get_db)):
+    contacts = db.query(Contact).all()
+    return contacts
+
+
+# Ручка для получения одного контакта по идентификатору
+@app.get("/contacts/{contact_id}", response_model=ContactValidator)
+def get_contact(contact_id: int, db: Session = Depends(get_db)):
+    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+    if contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return contact
+
+
+# Ручка для обновления существующего контакта
+@app.put("/contacts/{contact_id}", response_model=ContactValidator)
+def update_contact(
+    contact_id: int, updated_contact: ContactValidator, db: Session = Depends(get_db)
+):
+    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+    if contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+
+    for field, value in updated_contact.dict().items():
+        setattr(contact, field, value)
+
+    db.commit()
+    db.refresh(contact)
+    return contact
+
+
+# Ручка для удаления контакта
+@app.delete("/contacts/{contact_id}", response_model=ContactValidator)
+def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+    contact = db.query(Contact).filter(Contact.id == contact_id).first()
+    if contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+
+    db.delete(contact)
+    db.commit()
+    return contact
 
 
 if __name__ == "__main__":
